d27eb7c741fdef98b905d726560fe510a592ee4e
Jay Ramani <ramanijay2004@outlook.com>
2023-10-11 16:22:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ mongoose.connect(process.env.NODE_APP_CONNECTION, {
         const fileUpload = require('express-fileupload');
 
         app.use('/images/faculties', express.static('public/files'))
-
+        app.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            next();
+        })
+        
         app.use(express.json());
 
         app.use(
